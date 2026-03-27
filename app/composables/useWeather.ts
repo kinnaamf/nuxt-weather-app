0d0a78697c17b5849ff5c3eb9a5267d6ffff803b
kinnaamf <dimoje.1337@gmail.com>
2026-03-27 23:02:23 +0200

    feat: add weather data in localStorage

--- app/composables/useWeather.ts
+++ app/composables/useWeather.ts
@@ -1,7 +1,13 @@
 import type { Weather } from "~/types/weather";
 
 export const useWeather = () => {
-  const weatherData = useState<Weather | null>("weatherData", () => null);
+  const weatherData = useState<Weather | null>("weatherData", () => {
+    if (import.meta.client) {
+      const cached = localStorage.getItem("weather");
+      return cached ? JSON.parse(cached) : null;
+    }
+  });
+
   const { getCoordinates } = useGeolocation();
   const config = useRuntimeConfig()
 
@@ -12,13 +18,21 @@ export const useWeather = () => {
   const longitude = ref();
 
   const getUserWeather = async () => {
+    if (weatherData.value) return;
+
     await getCoordinates().then((coordinates) => {
       latitude.value = coordinates.latitude;
       longitude.value = coordinates.longitude;
     });
     try {
-      weatherData.value = await $fetch(`${ BASE_URL }${ latitude.value },${ longitude.value }?key=${ API_KEY }`);
+      const response = await $fetch<Weather>(`${ BASE_URL }${ latitude.value },${ longitude.value }?key=${ API_KEY }`);
       console.log(weatherData.value);
+
+      weatherData.value = response;
+
+      if (import.meta.client) {
+        localStorage.setItem("weather", JSON.stringify(response));
+      }
     } catch (error) {
       console.error(error);
     }
