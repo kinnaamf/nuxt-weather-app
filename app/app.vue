23a014a91dc96519c0dfd2ce6019abffc81aef9f
kinnaamf <dimoje.1337@gmail.com>
2026-03-28 18:38:11 +0200

    feat: add body attributes

--- app/app.vue
+++ app/app.vue
@@ -3,5 +3,10 @@
 </template>
 
 <script setup lang="ts">
-
+useHead({
+  bodyAttrs: {
+    style: 'background: linear-gradient(180deg, #143F70 0%, #283F58 39.9%, #335F91 100%);',
+    class: 'min-h-screen'
+  }
+})
 </script>
