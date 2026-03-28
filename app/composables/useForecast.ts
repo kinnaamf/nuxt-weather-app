cceaff98975e26dcc273170675403ebf1a908af2
kinnaamf <dimoje.1337@gmail.com>
2026-03-29 01:53:47 +0200

    feat: add 24 hours hours forecast

--- app/composables/useForecast.ts
+++ app/composables/useForecast.ts
@@ -16,8 +16,40 @@ export const useForecast = () => {
     return weekDays[dayIndex];
   }
 
+  const hourlyForecast = computed(() => {
+    if(!weatherData.value?.days?.length) return []
+    return weatherData.value?.days[0]?.hours || []
+  })
+
+  const next24hours = computed(() => {
+    if (!hourlyForecast.value?.length) return [];
+
+    const currentHour = new Date().getHours();
+
+    const startIndex = hourlyForecast.value.findIndex((hour) => {
+      const hourTime = parseInt(hour.datetime.split(':')[0]);
+      return hourTime >= currentHour;
+    });
+
+    const start = startIndex !== -1 ? startIndex : 0;
+
+    return hourlyForecast.value.slice(start, start + 24);
+  })
+
+  const formatTime = (timeString: string, index: number) => {
+    const hour = parseInt(timeString.split(':')[0]);
+    const currentHour = new Date().getHours();
+
+    if (index === 0) return 'Now';
+
+    return hour.toString().padStart(2, '0');
+  }
+
   return {
     weeklyForecast,
+    hourlyForecast,
     formatDate,
+    formatTime,
+    next24hours,
   }
 }
