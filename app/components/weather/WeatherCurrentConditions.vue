fa63c0f7ebfba90bfc9015cd3c2ae731dc9f1c50
kinnaamf <dimoje.1337@gmail.com>
2026-03-29 12:51:36 +0300

    feat: split weather conditions to component

--- app/components/weather/WeatherCurrentConditions.vue
+++ app/components/weather/WeatherCurrentConditions.vue
@@ -1,50 +1,27 @@
 <template>
-  <div class="flex flex-col gap-3">
-    <div class="flex justify-between w-full gap-3">
+  <div class="">
+    <div v-if="items.length" class="grid grid-cols-2 gap-3">
       <WeatherCard
-          title="Humidity"
-          :value="`${Math.round(current.humidity)}%`"
-          :description="humidityDescription(current.humidity ?? 0)"
-          :icon="getWeatherIcon('drop')"
-      />
-      <WeatherCard
-          title="Wind speed"
-          :value="`${Math.round(current?.windspeed)} kmh`"
-          :description="windSpeedDescription(current?.windspeed ?? 0)"
-          :icon="getWeatherIcon('wind')"
-      />
-    </div>
-    <div class="flex justify-between w-full gap-3">
-      <WeatherCard
-          title="Uv index"
-          :value="`${Math.round(current?.uvindex)}`"
-          :description="uvDescription(current?.uvindex ?? 0)"
-          :icon="getWeatherIcon('clear-day')"
-      />
-      <WeatherCard
-          title="Pressure"
-          :value="`${Math.round(current?.pressure)} hPa`"
-          :description="pressureDescription(current?.pressure ?? 0)"
-          :icon="getWeatherIcon('pressure')"
+        v-for="(item, index) in items"
+        :key="index"
+        :title="item.title"
+        :value="item.value"
+        :description="item.description"
+        :icon="item.icon"
       />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useDescription } from "~/types/useDescription";
-
-const { weatherData } = useWeather()
-const { getWeatherIcon } = useIcons()
-
-const {humidityDescription, temperatureDescription,
-  dewDescription, snowDescription,
-  snowDepthDescription, uvDescription,
-  solarDescription, visibilityDescription,
-  precipDescription, pressureDescription,
-  windSpeedDescription, } = useDescription();
-
-const current = computed(() => weatherData.value?.currentConditions)
+defineProps<{
+  items: {
+    title: string;
+    description: string;
+    icon: any;
+    value: any;
+  }
+}>()
 </script>
 
 <style scoped lang="postcss">
