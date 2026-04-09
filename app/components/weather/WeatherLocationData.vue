a511f544d5984634bb04ca30f8b9aac9a8765b43
kinnaamf <dimoje.1337@gmail.com>
2026-04-09 23:02:48 +0300

    fix: add text-center class to location

--- app/components/weather/WeatherLocationData.vue
+++ app/components/weather/WeatherLocationData.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="flex items-center gap-1 opacity-70 mb-1">
-    <LucideMapPin :size="14"/>
-    <span>{{ `${locationData?.city}, ${locationData?.country}` }}</span>
-  </div>
-  <div>
-    <h2 class="text-4xl font-black">{{ locationData?.city }}</h2>
+  <div class="text-center mx-2">
+    <div class="flex items-center gap-1 opacity-70 mb-1">
+      <LucideMapPin :size="14"/>
+      <span>{{ `${locationData?.city}, ${locationData?.country}` }}</span>
+    </div>
+    <div class="">
+      <h2 class="text-4xl font-black">{{ locationData?.city }}</h2>
+    </div>
   </div>
 </template>
 
